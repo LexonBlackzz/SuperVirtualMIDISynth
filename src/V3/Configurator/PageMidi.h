23297aebd17784980a19dc3af83d151009032ac8
LexonBlackzz <98592761+LexonBlackzz@users.noreply.github.com>
2026-08-17 20:45:58 +0300

    fix MIDI page declaration namespace

--- src/V3/Configurator/PageMidi.h
+++ src/V3/Configurator/PageMidi.h
@@ -1,10 +1,9 @@
 #ifndef SVMS_CONFIGURATOR_PAGEMIDI_H
 #define SVMS_CONFIGURATOR_PAGEMIDI_H
 
-struct ConfigDocument;
-
 namespace svms::cfg {
 
+class ConfigDocument;
 void DrawMidiPage(ConfigDocument& doc);
 
 } // namespace svms::cfg
